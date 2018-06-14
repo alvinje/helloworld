99707b1075877f7cc6939ded3e2ea1add4f2b1b6
Jérémie Alvin <je.alvin@orange.fr>
2018-06-14 17:23:30 +0200

    class with method and constructor version

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,31 @@
-print("Hello World !")
+import random
+import math
+
+
+class PiMonteCarlo:
+    def __init__(self, iteration):
+        self.iteration = iteration
+
+    def computePi(self):
+
+        total = 0
+
+        for i in range(1, self.iteration) :
+            x = random.random()
+            y = random.random()
+            dist = math.sqrt(x * x + y * y)
+
+            if dist <= 1:
+                total = total + 1
+
+        pi = total / self.iteration * 4
+
+        return pi
+
+
+myClass99 = PiMonteCarlo(99)
+myClass999 = PiMonteCarlo(999)
+pi99 = myClass99.computePi()
+pi999 = myClass999.computePi()
+
+print(pi99, pi999)
