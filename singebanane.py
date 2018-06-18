598f4906387cd4ef0ca69f7ba3e5fee656b04104
Jérémie Alvin <je.alvin@orange.fr>
2018-06-18 09:53:39 +0200

    'fibonnachi'

--- singebanane.py
+++ singebanane.py
@@ -1,24 +1,68 @@
 import random
 import math
 
-
 class Singe:
     def __init__(self, prenom):
         self.prenom = prenom
 
-    def mange(self):
+    def mange(self, banane):
         if self.prenom == "Pierre":
-            result = self.prenom + ' mange une banane jaune'
+            result = self.prenom + ' mange une banane ' + banane.couleur
         elif self.prenom == "Marie":
-            result = self.prenom + ' mange une banane verte'
+            result = self.prenom + ' mange une banane ' + banane.couleur
         else:
             result = 'Autre banane'
 
-        return result
+        print(result)
+
+    def sereproduit(self, tchoin, enfant):
+        enfantclass = Singe(enfant)
+        print(self.prenom + " s'est reproduit avec " + tchoin.prenom + " et on appeler leur enfant " + enfant)
+        return enfantclass
 
 
-prenoms = ["Pierre", "Marie"]
+class Banane:
+    def __init__(self, couleur):
+        self.couleur = couleur
 
-for val in prenoms:
-    myClass = Singe(val)
-    print(myClass.mange())
+
+class Graph:
+
+    def __init__(self):
+        self.noeud = []
+        self.arrete = []
+
+    def ajouternoeud(self, noeud):
+        self.noeud.append(noeud)
+        print(noeud + " est ajoute ")
+
+    def ajouterarete(self, noeud1, noeud2):
+        self.arrete.append(noeud1 + ':' + noeud2)
+        print("Arrete ajoutee entre " + noeud1 + " et " + noeud2)
+
+    def existarrete(self, noeud1, noeud2):
+
+        for i in self.arrete:
+            if i == noeud1 + ':' + noeud2:
+                return True
+            return False
+
+
+pierre = Singe("Pierre")
+marie = Singe("Marie")
+
+bananeVerte = Banane("verte")
+bananeJaune = Banane("jaune")
+
+pierre.mange(bananeJaune)
+marie.mange(bananeVerte)
+
+robert = pierre.sereproduit(marie, "robert")
+
+
+myGraph = Graph()
+myGraph.ajouternoeud('noeud1')
+myGraph.ajouternoeud('noeud2')
+myGraph.ajouterarete('noeud1', 'noeud2')
+print(myGraph.existarrete('noeud1', 'noeud2'))
+
---
+++ user_input.py
@@ -0,0 +1,22 @@
+def addition(n):
+    somme = 0
+    for i in range(1, n + 1):
+        somme = somme + i
+    print(somme)
+
+
+def fibonnaci():
+    n2 = 0
+    n1 = 1
+    for i in range(1, N):
+        V = n1 + n2
+        n2 = n1
+        n1 = V
+        print(V)
+
+
+
+# input = int(input("Donnez un entier : "))
+# addition(input)
+N = int(input("Entrer le maximum de la suite : "))
+fibonnaci(N)
